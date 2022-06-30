0e4126e4fbdf5e7681af6347190b03da776a29c5
maverick0395 <maverick032695@gmail.com>
2022-06-30 10:16:15 +0300

    [U]: minor fixes

--- features/environment.py
+++ features/environment.py
@@ -7,7 +7,7 @@ from tests.utils import populate_posts, populate_users
 def before_scenario(context, feature):
     app: Flask = create_app(environment="testing")
     app.config["TESTING"] = True
-    
+
     context.app_ctx = app.app_context()
     context.app_ctx.push()
     db.drop_all()
